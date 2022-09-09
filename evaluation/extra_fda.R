f4cfc32d68b6c11bbdde59cd51f9027bbcba34a5
Alessio Piraccini <101970801+apiraccini@users.noreply.github.com>
2022-09-09 15:38:07 +0200

    Add files via upload

---
+++ evaluation/extra_fda.R
@@ -0,0 +1,170 @@
+### extra - functional analysis
+
+rm(list = ls())
+set.seed(42)
+opar <- par()
+
+
+# libraries --------------------------------------------------------------------
+
+library(fda)
+library(refund)
+library(tidyverse)
+
+
+# utils ------------------------------------------------------------------------
+
+proj_dir <- "~/R_Jobs/StatAziende"
+if (normalizePath(getwd()) != normalizePath(proj_dir)) setwd(proj_dir)
+
+
+# read data --------------------------------------------------------------------
+
+load("data/df.RData")
+
+str(df)
+df <- as_tibble(df)
+
+
+# functional analysis ----------------------------------------------------------
+
+# campiono 3000 impianti
+set.seed(42)
+ids <- sample(unique(df$idImpianto), 3000, F)
+
+# ottengo dati sul prezzo in formato matriciale sett*impianto, convariate relative
+# a bandiera e regione
+prezzo <- df %>% 
+  filter(idImpianto %in% ids) %>% 
+  select(idImpianto, Regione, Bandiera, Settimana, Prezzo) %>%
+  arrange(Settimana) %>% 
+  pivot_wider(names_from = "Settimana", values_from = "Prezzo")
+data <- covmat <- prezzo %>% select(idImpianto, Regione, Bandiera) %>% as.data.frame()
+y <- prezzo %>% select(-idImpianto, -Regione, -Bandiera) %>% as.matrix()
+
+ids <- data$idImpianto
+prezzo <- t(y)
+settimana <- 1:52
+
+# plotting function
+ggmatplot <- function(d, col = "coral", tit = NULL){
+  d <- reshape2::melt(d, id="x")
+  p <- ggplot(d, aes_string(x="Var1", y="value", group="Var2")) +
+    geom_line(colour = col, alpha = 0.5) +
+    labs(x = "Settimana", y = "", title = tit) +
+    theme(legend.position = "none")
+  p
+}
+#ggmatplot(prezzo)
+
+# funzioni base
+order <- 6
+nbasis <- length(settimana) + order - 2
+basis_prezzo <- create.bspline.basis(range(settimana), nbasis, order, settimana)
+
+# penalita per il lisciamento
+Lfd.degree <- 4
+fdPar <- fdPar(basis_prezzo, Lfd.degree, lambda = 5)
+
+# lisciamento e storage oggetto fd
+prezzo_fd <- smooth.basis(settimana, prezzo, fdPar)$fd
+plot(prezzo_fd)
+prezzo_smooth <- eval.fd(settimana, prezzo_fd)
+data$prezzo_smooth <- t(prezzo_smooth)
+
+# grafici
+par(mfrow = c(2, 1), mar = rep(2, 4))
+prezzo %>% matplot(type = "l", main = "Dati originali")
+prezzo_smooth %>% matplot(type = "l", main = "Dati lisciati tramite B-splines")
+par(opar)
+
+prezzo_smooth %>% matplot(type = "l", col = "lightgrey", main = "Andamento del prezzo del gasolio nel 2021", xlab = "Settimana", ylab = "")
+lines(apply(prezzo_smooth, 1, mean), col = 2, lwd = 2)
+
+# derivata prima e seconda
+D1 <- int2Lfd(1)
+D2 <- int2Lfd(2)
+prezzo_d1 <- deriv.fd(prezzo_fd, D1) %>% eval.fd(settimana, .)
+prezzo_d2 <- deriv.fd(prezzo_fd, D2) %>% eval.fd(settimana, .)
+par(mfrow = c(2, 1), mar = rep(2,4))
+matplot(prezzo_d1, type = "l", col = "lightgrey", main = "Velocità (D1)", xlab = "Settimana", ylab = "") # derivata prima (crescita)
+lines(apply(prezzo_d1, 1, mean), col = 2, lwd = 2)
+abline(h = 0, lty = 2)
+matplot(prezzo_d2, type = "l", col = "lightgrey", main = "Accelerazione (D2)", xlab = "Settimana", ylab = "") # derivata prima (crescita)
+lines(apply(prezzo_d2, 1, mean), col = 2, lwd = 2)
+par(opar)
+
+# boxplot
+fbp <- boxplot(prezzo_fd)
+plot(fbp$depth)
+
+# covarianza
+cov.h <- var.fd(prezzo_fd)
+str(cov.h) 
+
+grid <- seq(1, 52, len = 100)
+varcov.h <- eval.bifd(grid, grid, cov.h)
+persp(grid, grid, varcov.h, theta = -45, phi = 25, r = 3, expand = 0.5, ticktype = "detailed",
+      xlab = "Tempo", ylab = "Tempo", zlab = "Covarianza prezzi")
+contour(grid, grid, varcov.h, xlab = "Tempo", ylab = "Tempo", main = "Covarianza prezzi")
+image(grid, grid, varcov.h, xlab = "Tempo", ylab = "Tempo", main = "Covarianza prezzi")
+
+# f-PCA (chiamate armoniche --> definiscono la miglior base per rappresentare i dati)
+ncomp <- 4
+prezzo_pca <- pca.fd(prezzo_fd, ncomp)
+
+print(prezzo_pca)$harmonics
+str(prezzo_pca)
+
+par(mfrow = c(2,2), mar = rep(2, 4))
+plot(prezzo_pca, cex.main = 0.9)
+par(opar)
+
+plot(prezzo_pca$harmonics)
+
+# bande di confidenza simultanee per la media
+# library(SCBmeanfd)
+# set.seed(42)
+# h <- cv.select(settimana, t(prezzo_smooth), degree = 1, interval = c(0.2, 1), gridsize = 100)
+# scby <- scb.mean(settimana, t(prezzo_smooth), bandwidth = h, scbtype = "both", gridsize = 100)
+# plot(scby)
+
+# NB!!!! usare fd2funData e autoplot (ha metodo per funData)
+
+# modeling ---------------------------------------------------------------------
+
+str(data)
+data$Regione <- factor(data$Regione)
+
+# m1
+m <- fosr(prezzo_smooth ~ Bandiera, method = "OLS", data = data)
+plot(m, split = 1, ylabel = "", titles = levels(data$Bandiera))
+par(opar)
+rsq <- 1- colSums((y - m$yhat)^2)/colSums((y - colMeans(y))^2)
+plot(rsq, type = "l", col = "coral", lwd = 3, main = paste0("Functional R2 (mean: ",  round(mean(rsq), 4), ")"))
+
+matplot(prezzo_smooth, type = "l", col = "light grey")
+matlines(t(m$yhat), type = "l", lty = 1)
+matplot(t(m$yhat), type = "l", lty = 1)
+
+# m2
+m2 <- fosr(prezzo_smooth ~ Regione, method = "OLS", data = data)
+plot(m2, split = 1, ylabel = "", titles = levels(data$Regione))
+par(opar)
+rsq <- 1- colSums((y - m2$yhat)^2)/colSums((y - colMeans(y))^2)
+plot(rsq, type = "l", col = "coral", lwd = 3, main = paste0("Functional R2 (mean: ",  round(mean(rsq), 4), ")"))
+
+matplot(settimana, prezzo_smooth, type = "l", col = "light grey")
+matlines(settimana, t(m2$yhat), type = "l", lty = 1)
+matplot(settimana, t(m2$yhat), type = "l", lty = 1)
+
+# m3
+m3 <- fosr(prezzo_smooth ~ Bandiera + Regione, method = "OLS", data = data)
+plot(m3, split = 1, ylabel = "", titles = c(levels(data$Bandiera), levels(data$Regione)))
+layout(1)
+rsq <- 1- colSums((y - m3$yhat)^2)/colSums((y - colMeans(y))^2)
+plot(rsq, type = "l", col = "coral", lwd = 3, main = paste0("Functional R2 (mean: ",  round(mean(rsq), 4), ")"))
+
+matplot(prezzo_smooth, type = "l", col = "light grey")
+matlines(t(m3$yhat), type = "l", lty = 1)
+matplot(t(m3$yhat), type = "l", lty = 1)
---
+++ evaluation/pred_map.R
@@ -0,0 +1,159 @@
+### Visualize predictions on the map for a given model
+
+rm(list = ls())
+set.seed(42)
+
+
+# libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(ggpubr)
+
+
+# utils -------------------------------------------------------------------
+
+proj_dir <- paste0(getwd(), "/StatAziende")
+setwd(proj_dir)
+
+
+# read data ---------------------------------------------------------------
+
+load("data/df.RData")
+
+str(df)
+head(df)
+
+
+# read shapefiles ---------------------------------------------------------
+
+#install.packages('sf')
+library(sf)
+
+prov21 <- st_read("data/shapefiles/ProvCM01012021_g")
+head(prov21) # sigla e' la chiave
+prov21 <- prov21 %>% rename(Provincia = SIGLA)
+
+reg21 <- st_read("data/shapefiles/Reg01012021_g")
+head(reg21) # sigla e' la chiave
+df <- df %>% mutate(Regione = gsub("/.*", "", Regione))
+reg21 <- reg21 %>% rename(Regione = DEN_REG)
+reg21$Regione <- ifelse(reg21$Regione == "Friuli Venezia Giulia", "Friuli-Venezia Giulia", reg21$Regione)
+
+ggplot(data = prov21) + geom_sf()
+ggplot(data = reg21) + geom_sf()
+
+
+# load model(s) -----------------------------------------------------------
+
+load("code/modeling/models/additivi.RData")
+
+to_map <- df %>% select(idImpianto, Settimana, Longitudine, Latitudine, Provincia, Regione, Prezzo, fitted, y)
+to_map$fitted2 <- fitted(m.gam1)
+to_map$Prezzo.previsto <- df$fitted + to_map$fitted2
+
+# by idImpianto
+map1 <- to_map %>%
+  group_by(idImpianto) %>%
+  summarise(Longitudine = first(Longitudine), Latitudine = first(Latitudine),
+            Provincia = first(Provincia), Regione = first(Regione),
+            Prezzo = mean(Prezzo), Prezzo.previsto = mean(Prezzo.previsto)) %>% 
+  st_as_sf(coords = c("Longitudine", "Latitudine"), crs = 4326)
+
+map1_plot <- ggplot() +
+  geom_sf(data = map1, aes(color = Prezzo.previsto)) +
+  scale_color_distiller(palette = "Reds", direction = 1) +
+  geom_sf(data = reg21, fill = NA) + 
+  labs(title = "Previsione del prezzo del gasolio",
+       subtitle = "Modello additivo\nPrevisioni medie annuali per impianto") +
+  theme(legend.title=element_blank())
+map1_plot
+
+# by Regione
+map2 <- to_map %>% group_by(Regione) %>% summarise(Prezzo.previsto = mean(Prezzo.previsto))
+map2_plot <- right_join(reg21, map2, by = "Regione") %>% 
+  ggplot(aes(fill = Prezzo.previsto)) +
+  geom_sf() +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  #labs(title = "Previsione del prezzo del gasolio",
+  #     subtitle = "Modello additivo\nPrevisioni medie annuali per regione") + 
+  theme(legend.title=element_blank())
+map2_plot
+
+# by Provincia
+map3 <- to_map %>% group_by(Provincia) %>% summarise(Prezzo.previsto = mean(Prezzo.previsto))
+map3_plot <- right_join(prov21, map3, by = "Provincia") %>% 
+  ggplot(aes(fill = Prezzo.previsto)) +
+  geom_sf() +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  #labs(title = "Previsione del prezzo del gasolio",
+  #     subtitle = "Modello additivo\nPrevisioni medie annuali per provincia") +
+  theme(legend.title=element_blank())
+map3_plot
+
+
+### temporal
+names(to_map)
+prev_weekly <- to_map %>% 
+  group_by(Settimana) %>% 
+  summarise(prezzo_real = mean(Prezzo),
+            prezzo_prev = mean(Prezzo.previsto),
+            low = quantile(Prezzo.previsto, 0.025),
+            up = quantile(Prezzo.previsto, 0.975)) %>% 
+  ggplot(aes(x = Settimana, y = prezzo_prev)) +
+  geom_ribbon(aes(ymin = low, ymax = up), alpha = .7, fill = "wheat") +
+  geom_line(col = "orange", size = 2) +
+  geom_line(aes(y = prezzo_prev-0.0005), col = "black", alpha = .7, linetype = "dashed") +
+  #geom_point(color = "coral") +
+  labs(x = "Settimana", y = "Prezzo previsto", title = "Andamento previsto del prezzo del gasolio", subtitle = "Anno 2021") +
+  theme(legend.position = "none",
+        plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14))
+
+prev_weekly
+
+
+# confronto previsti vs osservati - 1
+prev_weekly2 <- to_map %>% 
+  group_by(Settimana) %>% 
+  summarise(prezzo_real = mean(Prezzo),
+            prezzo_prev = mean(Prezzo.previsto)) %>% 
+  ggplot(aes(x = prezzo_real, y = prezzo_prev)) +
+  geom_point(color = "azure4", size = 3, alpha = 0.9) + 
+  geom_abline(intercept = 0, slope = 1, color = "coral", linetype = "dashed", size = 1.1, alpha = 0.5) +
+  labs(x = "Prezzo", y = "Prezzo previsto", title = "Confronto tra le medie settimanali per tutti gli impianti") +
+  theme(legend.position = "none",
+        plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14))
+
+prev_weekly2
+
+
+# confronto previsti vs osservati - 2
+ggplot(to_map, aes(x = Prezzo, y = Prezzo.previsto)) + 
+  geom_hex(bins = 40, color = "black", alpha = 0.7) +
+  geom_abline(slope = 1, intercept = 0, col = "green", linetype = "dashed", size = 1.3, alpha = 0.5) +
+  scale_fill_viridis_c(option = "B") + 
+  labs(x = "Prezzo", y = "Prezzo previsto",
+       title = "Confronto tra valori reali e previsti") +
+  theme(legend.position = "none",
+        plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 14))
+  
+
+# combine spatial plots
+library(ggpubr)
+p <- ggarrange(map2_plot, map3_plot)
+p <- annotate_figure(p, top = text_grob(paste0("Prezzo medio previsto del gasolio (€/l) \nAnno 2021, Media(sd): ", round(mean(to_map$Prezzo.previsto),3)," (", round(sd(to_map$Prezzo.previsto),2),")"), size = 16))
+p
+
+
+# salvataggio ------------------------------------------------------------------
+
+ggsave("code/evaluation/plots/map_reg_prov_21_previsti.pdf", plot = p, width = 9, height = 5)
+
+ggsave("code/evaluation/plots/prev_weekly.pdf", plot = prev_weekly, width = 7, height = 5)
+ggsave("code/evaluation/plots/prev_weekly2.pdf", plot = prev_weekly2, width = 7, height = 5)
+
+ggsave("code/evaluation/plots/mappa_previsioni_bestmodel_byImpianto.pdf", plot = map1_plot, width = 7, height = 5)
+ggsave("code/evaluation/plots/mappa_previsioni_bestmodel_byRegione.pdf", plot = map2_plot, width = 7, height = 5)
+ggsave("code/evaluation/plots/mappa_previsioni_bestmodel_byProvincia.pdf", plot = map3_plot, width = 7, height = 5)
